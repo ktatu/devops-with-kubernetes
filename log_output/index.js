0ba48ae5602935d32e603c06d67330795c8dc703
ktatu <kaikkonentatu@gmail.com>
2024-06-16 22:09:58 +0300

    1.07

--- log_output/index.js
+++ log_output/index.js
@@ -1,4 +1,7 @@
 const crypto = require("crypto")
+const express = require("express")
+const app = express()
+const config = require("./config")
 
 const randomString = crypto.randomBytes(20).toString("hex")
 
@@ -6,6 +9,14 @@ const logTimeStampAndString = () => {
     console.log(new Date() + ": " + randomString)
 }
 
+app.get("/", (req, res) => {
+    res.send(new Date() + ": " + randomString)
+})
+
 setInterval(() => {
     logTimeStampAndString()
 }, 5000)
+
+app.listen(config.PORT, () => {
+    console.log("Server running on port " + config.PORT)
+})
