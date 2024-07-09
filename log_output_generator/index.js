ff462f81822e1dda4ccbcf1078e6bccd33ddd03e
ktatu <kaikkonentatu@gmail.com>
2024-07-09 03:45:42 +0300

    2.04

--- log_output_generator/index.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs")
-
-const filePath = "/usr/src/app/files/timestamp/timestamp.txt"
-
-setInterval(() => {
-    const timestamp = new Date().toString()
-    fs.writeFileSync(filePath, timestamp)
-}, 5000)
